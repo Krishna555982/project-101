3bb3ac32622a0fc599e501aacbb506c29dce8ee7
Krishna555982 <95994492+Krishna555982@users.noreply.github.com>
2022-01-22 11:44:39 +0530

    create dbconnect.php

---
+++ dbconnect.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>registration</title>
+    </head>
+    <body> 
+        <?php
+        $conn = new mysql('localhost','root','123456','project101');
+        if($conn->connection_error)  {
+            die("connection failed: ".$conn->connect__error);
+        
+        }
+        $name=$_POST['name'];
+        $password=$_POST['password']
+        $email=$_POST['email']
+        $mobile number=$_POST['mobile number']
+        $gender=$_POST['gender']
+        $adress=$_POST['adress']
+        $sqlquery ="insert into details('$name','$password','$email','$mobile number','$gender','adress')";
+
+        if ($conn->query($sqlquery) === TRUE) {
+            echo  "<h1>recived your details<br/></h1>";
+        } else {
+            echo "error: ".$sql . "<br>" . $conn->error;
+        }
+        $conn->close();
+        ?>
+        </body>
+        </html>
